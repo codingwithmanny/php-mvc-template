64651392e10a8a47a6d9ee1c45a6dec44a118a3b
Manuel Pineault <manuel.pineault@gmail.com>
2016-03-11 22:59:43 -0500

    wip livesearch

--- public/assets/javascripts/scripts.js
+++ public/assets/javascripts/scripts.js
@@ -1,12 +1,129 @@
 $(document).ready(function(){
-    /* =====================================================================
-     MAKE NAV WHITE
-     ===================================================================== */
-    $(window).scroll(function(){
-        if($(document).scrollTop() > 100) {
-            $('#mainNav').addClass('white');
-        } else {
-            $('#mainNav').removeClass('white');
+    console.log('jQuery Loaded!');
+
+    /**
+     *
+     * @param str
+     * @returns {boolean}
+     */
+    function validate_json(str)
+    {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param url
+     * @param method
+     * @param payload
+     * @param callback
+     * @param failback
+     */
+    function ajax_request(method, url, payload, callback, failback) {
+        var options = {
+            type        : method,
+            url         : url,
+            dataType    : 'json',
+            processData : false,
+            cache       : false,
+            contentType : 'application/json',
+            beforeSend  : function(xhr) {
+                xhr.setRequestHeader('Accept', 'application/json');
+                xhr.setRequestHeader('WEBTOKEN', webtoken);
+            },
+            error       : function(jq_xhr, text_status) {
+                if(failback) {
+                    failback(jq_xhr, text_status);
+                }
+            },
+            success     : function(data, text_status, jq_xhr) {
+                if(callback) {
+                    callback(data, text_status, jq_xhr);
+                }
+            }
+        };
+
+        if(payload !== null && payload !== undefined && payload !== false && payload !== '') {
+            if (validate_json(payload)) {
+                options.data = JSON.stringify(payload);
+                options.contentType = 'application/json; charset=utf-8';
+            } else {
+                options.data = payload;
+            }
+        }
+
+        $.ajax(options);
+    }
+
+    //livesearch
+    $('input.livesearch').each(function(){
+        if($(this).attr('data-model') != null && $(this).attr('data-model') != '' && webtoken != '') {
+            var id = $(this).attr('name');
+            var model = $(this).attr('data-model');
+            var dataid = $(this).attr('data-id');
+            var datalabel = $(this).attr('data-label');
+            //console.log(parent);
+
+            //hide input
+            $(this).attr('type', 'hidden');
+
+            //add search
+            $(this).parent().append('' +
+                '<div class="dropdown">' +
+                '<input class="form-control" placeholder="Search for ' + id + '" type="search" id="' + id + '" value="" />' +
+                    '<ul id="' + id + '-menu" class="dropdown-menu" style="width: 100%;">' +
+                    '</ul>' +
+                '</div>');
+
+            //on keyup
+            $('#'+id).on('keyup', function(event){
+                var value = $(this).val();
+                $('#' + id + '-menu').html('');
+                if(value.length > 1) {
+                    $('#' + id + '-menu').append('<li class="text-center">Searching...</li>').show();
+                    ajax_request('get', '/'+model+'?q='+value+'&limit=5', null, function(data, text_status, jq_xhr) {
+                        if(data.data.length > 1) {
+                            $('#' + id + '-menu').html('');
+                            var li;
+                            for(var i = 0; i < data.data.length; i++) {
+                                li = '<li><a href="#" ';
+                                for(var x in data.data[i]) {
+                                    if(x == dataid) {
+                                        li += 'data-value="' + data.data[i][x] + '" ';
+                                    }
+                                    if(x == datalabel) {
+                                        li += 'data-label="' + data.data[i][x] + '" ';
+                                    }
+                                }
+                                li += '>';
+                                for(var x in data.data[i]) {
+                                    li += x + ': ' + data.data[i][x] + ' ';
+                                }
+                                li += '</a></li>';
+                                $('#' + id + '-menu').append(li);
+                            }
+                        } else {
+                            $('#' + id + '-menu').html('').append('<li class="text-center">No results</li>').show();
+                        }
+                    });
+                } else {
+                    $('#'+id+'-menu').hide();
+                }
+            });
+
+            $('form').on('click', '#'+id+'-menu a', function(){
+                var data = $(this).attr('data-value');
+                var label = $(this).attr('data-label');
+                $('#'+id).parent().append('<button class="btn btn-primary">'+label+' &times;</button>');
+                $('#' + id).val('').hide();
+                $('#' + id + '-menu').html('').hide();
+                return false;
+            });
         }
     });
 });
--- src/Models/ItemsModel.php
+++ src/Models/ItemsModel.php
@@ -26,7 +26,8 @@ class ItemsModel extends Model
      * @var array
      */
     protected $fields_required_options = [
-        'user_id' => ['type' => 'select', 'attributes' => ['class' => 'form-control', 'placeholder' => 'User Id']],
+//        'user_id' => ['type' => 'select', 'attributes' => ['class' => 'form-control', 'placeholder' => 'User Id']],
+        'user_id' => ['type' => 'text', 'attributes' => ['class' => 'livesearch form-control', 'placeholder' => 'User Id', 'data-model' => 'users', 'data-id' => 'id', 'data-label' => 'email']],
         'name' => ['type' => 'text', 'attributes' => ['min' => 8, 'max' => 250, 'class' => 'form-control', 'placeholder' => 'Name']],
         'description' => ['type' => 'textarea', 'attributes' => ['min' => 1, 'max' => 250, 'class' => 'form-control', 'placeholder' => 'Description']],
         'lat' => ['type' => 'number', 'attributes' => ['class' => 'form-control', 'placeholder' => 'Latitude']],
